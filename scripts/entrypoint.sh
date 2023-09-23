df4eef49593c3ffcb0734511fc443ddd10146304
tsamuel33 <tsamuel86@gmail.com>
2023-09-23 17:22:12 -0400

    fixed syntax for variable reference

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,21 +9,21 @@ else
 fi
 
 # Get Settings
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var branch_type --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var branch_type --config_file ${10})
 echo "branch_type=${SETTING}" >> "$GITHUB_OUTPUT"
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_environment --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_environment --config_file ${10})
 echo "github_environment=${SETTING}" >> "$GITHUB_OUTPUT"
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_secret_type --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_secret_type --config_file ${10})
 echo "github_secret_type=${SETTING}" >> "$GITHUB_OUTPUT"
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var policy_as_code_provider --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var policy_as_code_provider --config_file ${10})
 echo "policy_as_code_provider=${SETTING}" >> "$GITHUB_OUTPUT"
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var aws_account_number --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var aws_account_number --config_file ${10})
 echo "aws_account_number=${SETTING}" >> "$GITHUB_OUTPUT"
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var account_number_secret_name --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var account_number_secret_name --config_file ${10})
 echo "account_number_secret_name=${SETTING}" >> "$GITHUB_OUTPUT"
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_assumed_role_name --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_assumed_role_name --config_file ${10})
 echo "github_assumed_role_name=${SETTING}" >> "$GITHUB_OUTPUT"
-SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var change_set_before_deploy --config_file $10)
+SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var change_set_before_deploy --config_file ${10})
 echo "change_set_before_deploy=${SETTING}" >> "$GITHUB_OUTPUT"
 
 echo $GITHUB_OUTPUT
