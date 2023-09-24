1e126e4f24c6b8a4127632bb5e0397a9dbc0379c
tsamuel33 <tsamuel86@gmail.com>
2023-09-23 22:30:25 -0400

    update logic to get config settings

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,28 +9,16 @@ else
 fi
 
 # Get Settings
+setting_num=1
 for var in branch_type github_environment github_secret_type policy_as_code_provider aws_account_number account_number_secret_name github_assumed_role_name change_set_before_deploy
 do
-    SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var $var --config_file ${10} --action_mode docker)
-    echo "$var=${SETTING}" >> "$GITHUB_OUTPUT"
+    setting_num=$(($setting_num + 1))
+    if [ $1 == 'Setup '] || [ ${!setting_num} == '' ]
+        then
+            SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var $var --config_file ${10} --action_mode docker)
+            echo "$var=${SETTING}" >> "$GITHUB_OUTPUT"
+    fi
 done
-
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var branch_type --config_file ${10} --action_mode docker)
-# echo "branch_type=${SETTING}" >> "$GITHUB_OUTPUT"
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_environment --config_file ${10} --action_mode docker)
-# echo "github_environment=${SETTING}" >> "$GITHUB_OUTPUT"
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_secret_type --config_file ${10} --action_mode docker)
-# echo "github_secret_type=${SETTING}" >> "$GITHUB_OUTPUT"
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var policy_as_code_provider --config_file ${10} --action_mode docker)
-# echo "policy_as_code_provider=${SETTING}" >> "$GITHUB_OUTPUT"
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var aws_account_number --config_file ${10} --action_mode docker)
-# echo "aws_account_number=${SETTING}" >> "$GITHUB_OUTPUT"
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var account_number_secret_name --config_file ${10} --action_mode docker)
-# echo "account_number_secret_name=${SETTING}" >> "$GITHUB_OUTPUT"
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var github_assumed_role_name --config_file ${10} --action_mode docker)
-# echo "github_assumed_role_name=${SETTING}" >> "$GITHUB_OUTPUT"
-# SETTING=$(python3 -m scripts.env_setup --branch $BRANCH_NAME  --github_env_var change_set_before_deploy --config_file ${10} --action_mode docker)
-# echo "change_set_before_deploy=${SETTING}" >> "$GITHUB_OUTPUT"
 
 echo $GITHUB_OUTPUT
 cat $GITHUB_OUTPUT
